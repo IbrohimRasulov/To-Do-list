bf8c0ce4ef4a07d7a3f29a1435b1f95096c6353b
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-10-28 00:32:37 +0500

    Add function to add tasks to UI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,70 @@
-// import './style.css';
+import './style.css';
+
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const title = document.querySelector('#title');
+  const submitBtn = document.querySelector('#submit-btn');
+  const todoList = document.querySelector('.list-group');
+
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const li = document.createElement('li');
+    const checkBox = document.createElement('input');
+    const label = document.createElement('label');
+    const icon = document.createElement('i');
+
+    li.classList = 'list-group-item';
+    li.setAttribute('draggable', 'true');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.classList = 'checkbox';
+    label.innerText = title.value;
+    icon.classList = 'fa fa-ellipsis-v drag-drop float-end';
+
+    li.appendChild(checkBox);
+    li.appendChild(label);
+    li.appendChild(icon);
+    todoList.appendChild(li);
+
+    title.value = '';
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const submit = document.querySelector('#enter-task');
 // const taskList = document.querySelector('#task-list');
