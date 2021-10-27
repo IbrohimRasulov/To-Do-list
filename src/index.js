3fc4a503043c8f284bb5066f55014b1097573772
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-10-27 23:24:39 +0500

    Add minor style changes and make input required

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import './style.css';
+// import './style.css';
+
+// const submit = document.querySelector('#enter-task');
+// const taskList = document.querySelector('#task-list');
+// const clearBtn = document.querySelector('#clr-btn');
+// const taskInput = document.querySelector('#task-name');
+
+//   submit.addEventListener('click',(e) => {
+//     e.preventDefault();
+
+//   const li = document.createElement('li');
+//   const checkBox = document.createElement('input');
+//   const p = document.createElement('p');
+//   const icon = document.createElement('i');
+
+//   li.classList = 'list-group-item';
+//   li.setAttribute('dragdraggable', 'true');
+//   checkBox.setAttribute('type', 'checkbox');
+//   checkBox.classList = 'check-box';
+//   p.classList = 'd-inline';
+//   p.innerText = taskInput.value;
+//   icon.classList = 'fas fa-ellipsis-v drag-drop float-end';
+
+//   li.appendChild(checkBox);
+//   li.appendChild(p);
+//   li.appendChild(icon);
+//   taskList.appendChild(li);
+
+//   taskInput.value = '';
+//   });
