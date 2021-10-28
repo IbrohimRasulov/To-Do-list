8b260ee5d0232a37a21843aafe88326cf0cc2e1a
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-10-28 21:22:15 +0500

    Complete the project

--- src/index.js
+++ src/index.js
@@ -1,96 +1,57 @@
 import './style.css';
 
+const tasks = [
+  {
+    description: 'Set up a project with webpack',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Created an index.html file',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Set an array of sample to-do tasks',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'Create a funtion to populate the HTML',
+    completed: true,
+    index: 4,
+  },
+  {
+    description: 'Dynamically create list of tasks',
+    completed: true,
+    index: 5,
+  },
+  {
+    description: 'Created a style.css',
+    completed: true,
+    index: 6,
+  },
+];
+
+function printTasks(task) {
+  const li = document.createElement('li');
+  const checkBox = document.createElement('input');
+  const icon = document.createElement('i');
+  const label = document.createElement('label');
+
+  li.classList = 'list-group-item';
+  li.setAttribute('draggable', 'true');
+  checkBox.type = 'checkbox';
+  checkBox.classList = 'checkbox';
+  checkBox.checked = task.completed;
+  label.innerHTML = task.description;
+  icon.classList = 'fa fa-ellipsis-v drag-drop float-end';
 
 
+  li.appendChild(checkBox);
+  li.appendChild(label);
+  li.appendChild(icon);
+  document.querySelector('.list-group').appendChild(li);
+}
 
-window.addEventListener('DOMContentLoaded', () => {
-  const title = document.querySelector('#title');
-  const submitBtn = document.querySelector('#submit-btn');
-  const todoList = document.querySelector('.list-group');
-
-  submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const li = document.createElement('li');
-    const checkBox = document.createElement('input');
-    const label = document.createElement('label');
-    const icon = document.createElement('i');
-
-    li.classList = 'list-group-item';
-    li.setAttribute('draggable', 'true');
-    checkBox.setAttribute('type', 'checkbox');
-    checkBox.classList = 'checkbox';
-    label.innerText = title.value;
-    icon.classList = 'fa fa-ellipsis-v drag-drop float-end';
-
-    li.appendChild(checkBox);
-    li.appendChild(label);
-    li.appendChild(icon);
-    todoList.appendChild(li);
-
-    title.value = '';
-  })
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const submit = document.querySelector('#enter-task');
-// const taskList = document.querySelector('#task-list');
-// const clearBtn = document.querySelector('#clr-btn');
-// const taskInput = document.querySelector('#task-name');
-
-//   submit.addEventListener('click',(e) => {
-//     e.preventDefault();
-
-//   const li = document.createElement('li');
-//   const checkBox = document.createElement('input');
-//   const p = document.createElement('p');
-//   const icon = document.createElement('i');
-
-//   li.classList = 'list-group-item';
-//   li.setAttribute('dragdraggable', 'true');
-//   checkBox.setAttribute('type', 'checkbox');
-//   checkBox.classList = 'check-box';
-//   p.classList = 'd-inline';
-//   p.innerText = taskInput.value;
-//   icon.classList = 'fas fa-ellipsis-v drag-drop float-end';
-
-//   li.appendChild(checkBox);
-//   li.appendChild(p);
-//   li.appendChild(icon);
-//   taskList.appendChild(li);
-
-//   taskInput.value = '';
-//   });
+tasks.forEach((task) => printTasks(task));
