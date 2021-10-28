a551a10c7b4eb1f64cbfd5edd3f5f11c3805b682
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-10-28 22:30:10 +0500

    Correct linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+export const mode = 'development';
+export const entry = './src/index.js';
+export const devtool = 'inline-source-map';
+export const devServer = {
+  static: './dist',
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+];
+export const output = {
+  filename: 'main.js',
+  path: resolve(__dirname, 'dist'),
+  clean: true,
+  publicPath: '/',
+};
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    },
   ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
 };
